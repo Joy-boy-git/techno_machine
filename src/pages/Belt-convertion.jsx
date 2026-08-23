d9e8af3b9a2f2fa8912ee480c55af3b0475dab47
Joy-boy-git <berlinthorsp@gmail.com>
2026-08-23 22:01:57 +0530

    fixed

--- src/pages/Belt-convertion.jsx
+++ src/pages/Belt-convertion.jsx
@@ -15,7 +15,7 @@ import {
 
 import image1 from "../assets/AutoLCM-01.avif";
 import image2 from "../assets/Manual_LCM.avif";
-import image3 from "../assets/Mini_Belt_Slitting.avif";
+import image3 from "../assets/Mini_Belt_slitting.avif";
 import image4 from "../assets/Belt_Slitting_Machine.avif";
 import image5 from "../assets/Belt_Press.avif";
 import image6 from "../assets/Belt_Press_2.avif";
